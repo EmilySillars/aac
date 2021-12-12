4711e14c5586b84f7f21432ce9b011fb8d80e2d4
EmilySillars <emsillars@gmail.com>
2021-12-12 18:53:02 -0500

    add broken surjection func

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,7 +12,9 @@ import Data.List.Split
 import Data.Text (pack)
 import Data.Text.Encoding as TSE
 import Data.Word (Word8)
---import Data.Typeable (typeOf)
+import Data.Typeable (typeOf)
+import GHC.ExecutionStack (Location(functionName))
+import Text.Printf (IsChar(toChar))
 --import Data.Word
 --import Data.Array.Repa.Index
 --import Control.Monad
@@ -64,12 +66,102 @@ convert png =
           h = imageHeight imgRGB
           dim = "(" ++ show w ++ " x " ++ show h ++ ")\n"
           imgRepa = R.convertImage imgRGB :: Img RGB
-          bi = babaa
+          bi = babaa ++ "\n again but smaller: \n" ++ q
+          -- ++ "\n" ++ test ++ "\n" ++ test2 ++ "\n" ++ test3 
+                     -- ++ "\n" ++ m ++ n ++ "\nmatrixTest: \n" 
+                     -- ++ q ++ "\n matrixTest2:\n" ++ p
+        --  test3 = show $ toList t1 == toList t2
           babaa = intercalate "\n" $ chunksOf w $ toList $ bijection $ imgData imgRepa
+          moo = intercalate "\n" $ chunksOf w $ toList $ bijection $ imgData imgRepa
+        --   test = show (typeOf $ t1) ++ ": " ++ show (listOfShape $ extent t1)
+        --    --  Array D ((:.) ((:.) ((:.) Z Int) Int) Int) Word8
+        --   test2 = show (typeOf $ t2) ++ ": " ++ show (listOfShape $ extent t2)
+        --   t1 = imgData imgRepa
+        --   t2 = reshape (Z :. (32::Int) :. (32::Int) :. (4::Int) :. (3::Int)) $ imgData imgRepa
+        --   --foldS :: (Shape sh, Source r a, Unbox a) => 
+        --   -- (a -> a -> a) -> a -> Array r (sh :. Int) a -> Array U sh a
+        --   matrix = fromListUnboxed (Z :. (2::Int) :. (2::Int)) [1::Int,2,3,4] 
+        --   foldedMatrix = foldS (+) 0 matrix
+        --   m = show matrix ++ "\nfolded: " ++ show foldedMatrix
+        --   dumb =concat [[0::Int,0,0], [1,1,1], [2,2,2], [3,3,3],
+        --           [0,0,0], [1,1,1], [2,2,2], [3,3,3],
+        --           [0,0,0], [1,1,1], [2,2,2], [3,3,3],
+        --           [0,0,0], [1,1,1], [2,2,2], [3,3,3]]
+        --   dummyMatrix = 
+        --       fromListUnboxed (Z :. (2::Int) :. (2::Int) :. (4::Int) :. (3::Int)) dumb
+        --   foldedDummy = foldS (+) 0 dummyMatrix
+        --   n = "\ndummy " ++ show dummyMatrix ++ "\nfolded dummy: " ++ show foldedDummy
+          q = intercalate "\n" $ chunksOf 32 $ toList $ matrixTest $ imgData imgRepa
+         -- p = intercalate "\n" $ chunksOf 8 $ toList $ matrixTest2 $ imgData imgRepa
       (Left err) ->
         return $ "Read Error: " ++ err
 
--- convert png = do
+
+matrixTest :: Array D DIM3 Word8 -> Array D DIM2 Char
+matrixTest img = step4
+  where results = "\n" ++ show (typeOf start) ++ "\n" ++ show (typeOf step1) ++ 
+                  "\n" ++ show (typeOf step2) ++ " " ++ show (listOfShape $ extent step2) ++
+                  "\n" ++ show (typeOf step3)
+        -- break into tiles
+        start = reshape (Z :. (32::Int) :. (32::Int) :. (4::Int) :. (3::Int)) img
+        -- sum up pixels in each tile
+        step1 = toTriple start
+        toTriple :: Array D DIM4 Word8 -> Array D DIM3 (Double,Double,Double)
+        toTriple st = fromFunction (Z :. (32::Int) :. (32::Int) :. (4::Int)) triple
+          where triple = \(Z :. i :. j :. k) -> 
+                        let a = fromIntegral $ st ! (Z :. i :. j :. k :. 0)  in
+                        let b = fromIntegral $ st ! (Z :. i :. j :. k :. 1) in
+                        let c = fromIntegral $ st ! (Z :. i :. j :. k :. 2) in
+                        (a,b,c)
+        step2 = foldS (\(a,b,c) (v1,v2,v3) -> (a+v1, b+v2, c+v3)) (0,0,0) step1
+        -- divide each sum by number of pixels/tile
+        step3 = fromFunction (Z :. (32::Int) :. (32::Int)) func
+          where func = \(Z :. i :. j) -> 
+                        case step2 ! (Z :. i :. j) of
+                       (a,b,c) -> (a/4, b/4, c/4)
+        step4 = fromFunction (Z :. (32::Int) :. (32::Int)) toChar
+          where toChar = \(Z :. i :. j) -> 
+                         let g = gray $ step3 ! (Z :. i :. j) in
+                         ramp `B.index` (g `mod` 70)
+
+
+matrixTest2 :: Array D DIM3 Word8 -> Array D DIM2 Char
+matrixTest2 img = step4
+  where results = "\n" ++ show (typeOf start) ++ "\n" ++ show (typeOf step1) ++ 
+                  "\n" ++ show (typeOf step2) ++ " " ++ show (listOfShape $ extent step2) ++
+                  "\n" ++ show (typeOf step3)
+        -- break into tiles
+        start = reshape (Z :. (16::Int) :. (32::Int) :. (8::Int) :. (3::Int)) img
+        -- sum up pixels in each tile
+        step1 = toTriple start
+        toTriple :: Array D DIM4 Word8 -> Array D DIM3 (Double,Double,Double)
+        toTriple st = fromFunction (Z :. (16::Int) :. (32::Int) :. (8::Int)) triple
+          where triple = \(Z :. i :. j :. k) -> 
+                        let a = fromIntegral $ st ! (Z :. i :. j :. k :. 0)  in
+                        let b = fromIntegral $ st ! (Z :. i :. j :. k :. 1) in
+                        let c = fromIntegral $ st ! (Z :. i :. j :. k :. 2) in
+                        (a,b,c)
+        step2 = foldS (\(a,b,c) (v1,v2,v3) -> (a+v1, b+v2, c+v3)) (0,0,0) step1
+        -- divide each sum by number of pixels/tile
+        step3 = fromFunction (Z :. (16::Int) :. (32::Int)) func
+          where func = \(Z :. i :. j) -> 
+                        case step2 ! (Z :. i :. j) of
+                       (a,b,c) -> (a/8, b/8, c/8)
+        step4 = fromFunction (Z :. (16::Int) :. (32::Int)) toChar
+          where toChar = \(Z :. i :. j) -> 
+                         let g = gray $ step3 ! (Z :. i :. j) in
+                         ramp `B.index` (g `mod` 70)
+
+
+
+        -- now just need to take average, turn to gray, and make a character!!!
+             
+        -- toTriple :: (Int,Int,Int) -> Int -> (Int,Int,Int)
+        -- toTriple triple v = case triple of
+        --     (-1, -1, -1) -> (v, -1, -1)
+        --     (a, -1, -1) -> (a ,v, -1)
+        --     (a, b, -1) -> (a, b, -1)
+        --     (_,_,_) -> (-1,-1,-1)
 --     Right (ImageRGB8 v) <- readPng png
 --     "(" ++ show (imageWidth v) ++ " x " ++ show (imageHeight v) ++ ")"
 
@@ -126,4 +218,8 @@ bijection pixels = a --computeS a ::Array U DIM2 Word8
            in let b = fromIntegral $ pixels ! (Z :. i :. j :. 2)
                in ramp `B.index` (gray (r, g, b) `mod` 70)
 
--- surjection :: Array D DIM3 Word8 -> Array D DIM2 Char
+surjection :: Array D DIM3 Word8 -> Array D DIM2 Char
+surjection pixels = b
+  where
+       b = fromFunction (Z:.10:.1) (\(Z:.i:._) -> head $ show (i * 10))
+       a = pixels
